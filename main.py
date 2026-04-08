8a822babd0fb07d4f2c9b8759a8633876dceb128
geniousabby <abbyspam1228@gmail.com>
2026-04-08 14:09:46 -0700

    Pseudo code
    Added the base pseudo code. Will add more as i add more code.

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Encript or decript using the affine cipher.
+
+"""
+
+# Welcome user to the program
+
+
+
+# Print definition of the cipher
+
+
+# Ask user if they want to encrypt or decrypt
+
+
+# Print message
